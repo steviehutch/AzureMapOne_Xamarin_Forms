54215ab22d0ba71e646237dd44bc54512e365e01
hutch_000 <hutch_000@STUDY-PC>
2017-03-03 11:18:16 +0000

    Add project files.

---
+++ AzureMapOne/TodoList.xaml.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace AzureMapOne
+{
+    public partial class TodoList : ContentPage
+    {
+        TodoItemManager manager;
+
+        public TodoList()
+        {
+            InitializeComponent();
+
+            manager = TodoItemManager.DefaultManager;
+
+            // OnPlatform<T> doesn't currently support the "Windows" target platform, so we have this check here.
+            if (manager.IsOfflineEnabled &&
+                (Device.OS == TargetPlatform.Windows || Device.OS == TargetPlatform.WinPhone))
+            {
+                var syncButton = new Button
+                {
+                    Text = "Sync items",
+                    HeightRequest = 30
+                };
+                syncButton.Clicked += OnSyncItems;
+
+                buttonsPanel.Children.Add(syncButton);
+            }
+
+            var FalmouthLat = "50.1491811";
+            var FalmouthLong = "-5.0708545999999615";
+
+            MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(
+                    new Position(50.14, -5.07), Distance.FromMiles(1)));
+            // falmouth university lat, long =
+            // 50.1491811,-5.0708545999999615
+            var position = new Position(50.1491811, -5.0708545999999615); // Latitude, Longitude
+            var pin = new Pin
+            {
+                Type = PinType.Place,
+                Position = position,
+                Label = "MAP THIS !!!",
+                Address = "You have 15 seconds to move"
+            };
+
+            MyMap.Pins.Add(pin);
+            /*
+            var slider = new Slider(1, 18, 1);
+            slider.ValueChanged += (sender, e) => {
+                var zoomLevel = e.NewValue; // between 1 and 18
+                var latlongdegrees = 360 / (Math.Pow(2, zoomLevel));
+                MyMap.MoveToRegion(new MapSpan(MyMap.VisibleRegion.Center, latlongdegrees, latlongdegrees));
+            };*/
+
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            // Set syncItems to true in order to synchronize the data on startup when running in offline mode
+            await RefreshItems(true, syncItems: true);
+        }
+
+        // Data methods
+        async Task AddItem(TodoItem item)
+        {
+            await manager.SaveTaskAsync(item);
+            todoList.ItemsSource = await manager.GetTodoItemsAsync();
+        }
+
+        async Task CompleteItem(TodoItem item)
+        {
+            item.Done = true;
+            await manager.SaveTaskAsync(item);
+            todoList.ItemsSource = await manager.GetTodoItemsAsync();
+        }
+
+        public async void OnAdd(object sender, EventArgs e)
+        {
+            var todo = new TodoItem { Name = newItemName.Text };
+            await AddItem(todo);
+
+            newItemName.Text = string.Empty;
+            newItemName.Unfocus();
+        }
+
+        // Event handlers
+        public async void OnSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var todo = e.SelectedItem as TodoItem;
+            if (Device.OS != TargetPlatform.iOS && todo != null)
+            {
+                // Not iOS - the swipe-to-delete is discoverable there
+                if (Device.OS == TargetPlatform.Android)
+                {
+                    await DisplayAlert(todo.Name, "Press-and-hold to selection location " + todo.Name, "Got it!");
+                }
+                else
+                {
+                    // Windows, not all platforms support the Context Actions yet
+                    if (await DisplayAlert("Location selected?", "Do you wish to target this location ? " + todo.Name + "?", "Complete", "Cancel"))
+                    {
+                        await CompleteItem(todo);
+                    }
+                }
+            }
+
+            // prevents background getting highlighted
+            todoList.SelectedItem = null;
+        }
+
+        // http://developer.xamarin.com/guides/cross-platform/xamarin-forms/working-with/listview/#context
+        public async void OnComplete(object sender, EventArgs e)
+        {
+            var mi = ((MenuItem)sender);
+            var todo = mi.CommandParameter as TodoItem;
+            await CompleteItem(todo);
+        }
+
+        // http://developer.xamarin.com/guides/cross-platform/xamarin-forms/working-with/listview/#pulltorefresh
+        public async void OnRefresh(object sender, EventArgs e)
+        {
+            var list = (ListView)sender;
+            Exception error = null;
+            try
+            {
+                await RefreshItems(false, true);
+            }
+            catch (Exception ex)
+            {
+                error = ex;
+            }
+            finally
+            {
+                list.EndRefresh();
+            }
+
+            if (error != null)
+            {
+                await DisplayAlert("Refresh Error", "Couldn't refresh data (" + error.Message + ")", "OK");
+            }
+        }
+
+        public async void OnSyncItems(object sender, EventArgs e)
+        {
+            await RefreshItems(true, true);
+        }
+
+        private async Task RefreshItems(bool showActivityIndicator, bool syncItems)
+        {
+            using (var scope = new ActivityIndicatorScope(syncIndicator, showActivityIndicator))
+            {
+                todoList.ItemsSource = await manager.GetTodoItemsAsync(syncItems);
+            }
+        }
+
+        private class ActivityIndicatorScope : IDisposable
+        {
+            private bool showIndicator;
+            private ActivityIndicator indicator;
+            private Task indicatorDelay;
+
+            public ActivityIndicatorScope(ActivityIndicator indicator, bool showIndicator)
+            {
+                this.indicator = indicator;
+                this.showIndicator = showIndicator;
+
+                if (showIndicator)
+                {
+                    indicatorDelay = Task.Delay(2000);
+                    SetIndicatorActivity(true);
+                }
+                else
+                {
+                    indicatorDelay = Task.FromResult(0);
+                }
+            }
+
+            private void SetIndicatorActivity(bool isActive)
+            {
+                this.indicator.IsVisible = isActive;
+                this.indicator.IsRunning = isActive;
+            }
+
+            public void Dispose()
+            {
+                if (showIndicator)
+                {
+                    indicatorDelay.ContinueWith(t => SetIndicatorActivity(false), TaskScheduler.FromCurrentSynchronizationContext());
+                }
+            }
+        }
+    }
+}
+
